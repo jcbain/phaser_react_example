c439b0b3a2fb020fd355fdf5e825657054ff997d
jcbain <jamescbain@gmail.com>
2020-10-23 11:27:01 -0600

    add a stupid sample scene

--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
 import Phaser from 'phaser';
 
+import ExampleScene from "./scenes/examplescene";
+
 const Game = () => {
     const config = {
         type: Phaser.AUTO,
         width: 800,
         height:800,
         parent: "phaser-game",
+        scene: [ExampleScene]
     }
 
     useEffect(() => {
---
+++ src/scenes/examplescene.js
@@ -0,0 +1,14 @@
+import { Scene } from "phaser";
+
+export default class ExampleScene extends Scene {
+  create() {
+    const text = this.add.text(250, 250, "Toggle UI", {
+      backgroundColor: "white",
+      color: "blue",
+      fontSize: 48
+    });
+
+    text.setInteractive({ useHandCursor: true });
+
+}
+}
